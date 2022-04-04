f0554db84e60489ae6aa85f8c5c6ce879141050e
John Torres <apodpm@gmail.com>
2022-04-04 16:58:15 +0800

    remove debug.js

--- debug.js
+++
@@ -1,8 +0,0 @@
-const commandLineArgs = require('command-line-args')
-const options = commandLineArgs([
-    { name: 'verbose', alias: 'v', type: Boolean, defaultOption: false },
-    { name: 'src', type: String, multiple: true, defaultOption: true },
-    { name: 'timeout', alias: 't', type: Number }
-])
-
-console.log(options)
