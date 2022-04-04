e6f82a5233a8c9ded94ffe1e3acded16a0022030
John Torres <apodpm@gmail.com>
2022-04-04 17:09:19 +0800

    update README.md

--- eth.js
+++ eth.js
@@ -5,38 +5,41 @@ const BIP39 = require('bip39')
 const HD_KEY = require('ethereumjs-wallet').hdkey
 
 const ROOT_PATH = "m/44'/60'/0'/0/"
-const FILENAME = `${moment().format('YYYYMMDD_HHmmss')}.txt`
+
 const OPTIONS = commandLineArgs([
-    {name: 'number', alias: 'n', type: Number}
+    {name: 'number', alias: 'n', type: Number},
+    {name: 'output', alias: 'o', type: String}
 ])
 
-const COUNT = OPTIONS.number || 20
+const COUNT = OPTIONS['number'] || 20
+
+const FILENAME = OPTIONS['output'] || `${moment().format('YYYYMMDD_HHmmss')}.txt`
 
 const save = function save(data) {
     fs.appendFileSync(`./output/${FILENAME}`, data, 'utf8')
 }
 
-const main = function () {
-    const mnemonic = BIP39.generateMnemonic(128)
+const mnemonic = BIP39.generateMnemonic(128)
 
-    save(mnemonic + '\n')
-    console.log(mnemonic)
+save(mnemonic + '\n\n')
+console.log(`\n${mnemonic}\n`)
 
-    const seed = BIP39.mnemonicToSeedSync(mnemonic)
+const seed = BIP39.mnemonicToSeedSync(mnemonic)
 
-    const hdWallet = HD_KEY.fromMasterSeed(seed)
+const hdWallet = HD_KEY.fromMasterSeed(seed)
 
-    for (let i = 0; i < COUNT; i++) {
-        const wallet = hdWallet.derivePath(ROOT_PATH + i).getWallet()
+for (let i = 0; i < COUNT; i++) {
+    const wallet = hdWallet.derivePath(ROOT_PATH + i).getWallet()
 
-        const privateKey = wallet.getPrivateKeyString()
-        const address = wallet.getChecksumAddressString()
+    const privateKey = wallet.getPrivateKeyString()
+    const address = wallet.getChecksumAddressString()
 
-        const line = `#${i}: ${address} <= ${privateKey}`
+    const line = `#${i}: ${address} <= ${privateKey}`
 
-        save(line + '\n')
-        console.log(line)
-    }
+    save(line + '\n')
+    console.log(line)
 }
 
-main()
+
+console.log(`\n\noutput file: ./output/${FILENAME}`)
+
